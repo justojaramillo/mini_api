1951b16979758b7f8585f45d2a39545988e18fac
justojaramillo <justojaramillo1991@gmail.com>
2025-04-13 08:25:42 -0500

    methods get, post,delete, patch

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 const router = express.Router();
 
-const users = [
+let users = [
   {
     user_id: '14f19048-3102-412a-94e9-73576d2449fb',
     first_name: 'John',
@@ -22,11 +22,39 @@ router.get('/', (req, res) => {
   res.send(users);
 });
 
+router.get('/:user_id', (req, res) => {
+  const { user_id } = req.params;
+  const user = users.find((user) => user.user_id == user_id);
+  res.send(user);
+});
+
 router.post('/', (req, res) => {
   const user = { user_id: uuidv4(), ...req.body };
   users.push(user);
   res.send(user);
   res.send(`User with name ${user.first_name} added to the database!`);
+});
+
+router.delete('/:user_id', (req, res) => {
+  const { user_id } = req.params;
+  users = users.filter((user) => user.user_id !== user_id);
+  res.send(users);
+});
+
+router.patch('/:user_id', (req, res) => {
+  const { user_id } = req.params;
+  const { first_name, last_name, age } = req.body;
+  let user = users.find((user) => user.user_id == user_id);
+  if (first_name) {
+    user.first_name = first_name;
+  }
+  if (last_name) {
+    user.last_name = last_name;
+  }
+  if (age) {
+    user.age = age;
+  }
+  res.send(`User with user_id ${user_id} has been updated`);
 });
 
 export default router;
